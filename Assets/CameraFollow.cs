0ff7087bd330cc5965f94d023574a5f98cff2c96
Dennis Rübner <dennis.ruebner@gmail.com>
2017-08-14 20:03:34 +0200

    Parkour built and Camera foolow script added

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    #region Variables
+
+    private GameObject player;
+
+#endregion
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
