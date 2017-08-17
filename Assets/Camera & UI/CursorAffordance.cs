5012bcafcb5e27c3598de2c5b0c8f0018df287a6
Dennis Rübner <dennis.ruebner@gmail.com>
2017-08-17 21:22:17 +0200

    Added Attackrange and player health bar and attack scenario documentation

--- Assets/Camera & UI/CursorAffordance.cs
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CameraRaycaster))]
 public class CursorAffordance : MonoBehaviour {
 
     [SerializeField] Texture2D walkCursor = null;
     [SerializeField] Texture2D attackCursor = null;
     [SerializeField] Texture2D unknownCursor = null;
 
-    [SerializeField] Vector2 cursorHotspot = new Vector2(96,96);
+    [SerializeField] Vector2 cursorHotspot = new Vector2(0,0);
 
     CameraRaycaster cameraRaycaster;
 
@@ -16,13 +17,13 @@ public class CursorAffordance : MonoBehaviour {
     void Start () {
 
         cameraRaycaster = GetComponent<CameraRaycaster>();
+        cameraRaycaster.onLayerChange += OnLayerChanged;
 
     }
 	
-	// Update is called once per frame
-	void Update () {
+	void OnLayerChanged (Layer givenLayer) { //Only called when layer changed
 
-        switch (cameraRaycaster.layerHit)
+        switch (givenLayer)
         {
             case Layer.Walkable:
                 Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,49 +5,112 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float walkMoveStopRadius = 0.2f;
 
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+    [SerializeField] float attackMoveStopRadius = 5f;
+
+    ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    Vector3 currentDestination, clickPoint;
         
+    bool isInDirectMode = false;
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-        m_Character = GetComponent<ThirdPersonCharacter>();
-        currentClickTarget = transform.position;
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+        currentDestination = transform.position;
     }
 
-    // TODO fix issue with combining click to move and WASD speed
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) //G for gamepad TODO allow remap later or change in menu
+        {
+            isInDirectMode = !isInDirectMode;
+            currentDestination = transform.position; // reset click position
+            print("Controlmode Controller: " + isInDirectMode);
+        }
+    }
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+        if(isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement(); // Mouse movement
+        }
+
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical"); ;
+
+        // calculate camera relative direction to move:
+        Vector3 CameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = v * CameraForward + h * Camera.main.transform.right;
+
+        thirdPersonCharacter.Move(movement, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+            clickPoint = cameraRaycaster.hit.point;
+
             switch (cameraRaycaster.layerHit)
             {
                 case Layer.Walkable:
-                    currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+                    currentDestination = ShortDestination(clickPoint, walkMoveStopRadius);  // So not set in default case
                     break;
                 case Layer.Enemy:
-                    print("Enemy clicked!");
+                    currentDestination = ShortDestination(clickPoint, attackMoveStopRadius);  // run not as far when attacking
                     break;
                 default:
                     print("unexpected layer clicked");
                     return;
             }
         }
-        var playerToClickPoint =  currentClickTarget - transform.position;
-        if(playerToClickPoint.magnitude >= walkMoveStopRadius)
+
+        WalkToDestination();
+    }
+
+    private void WalkToDestination()
+    {
+        var playerToClickPoint = currentDestination - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
         {
-            m_Character.Move(currentClickTarget - transform.position, false, false);
+            thirdPersonCharacter.Move(currentDestination - transform.position, false, false);
         }
         else
         {
-            m_Character.Move(Vector3.zero, false, false);
+            thirdPersonCharacter.Move(Vector3.zero, false, false);
         }
+    }
+
+    private Vector3 ShortDestination(Vector3 destination, float shortening)
+    {
+        Vector3 reductionVector = (destination - transform.position).normalized * shortening;
+        return destination - reductionVector;
+    }
+
+    private void OnDrawGizmos()
+    {   
+        //Draw movement gizmos
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position, clickPoint);
+        Gizmos.DrawSphere(currentDestination, 0.1f);
+        Gizmos.DrawSphere(clickPoint, 0.15f);
+
+        //Draw attack gizmos
+        Gizmos.color = new Color(255f, 0f, 0f, 0.5f);
+        Gizmos.DrawWireSphere(transform.position, attackMoveStopRadius);
     }
 }
 
