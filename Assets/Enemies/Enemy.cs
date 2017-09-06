3395b4c3812f4de7689ab857d79f8f1f89a1ade2
Dennis Rübner <dennis.ruebner@gmail.com>
2017-09-06 21:16:11 +0200

    Enemy Projectiles added firing at player, plus damaging him

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -7,8 +7,12 @@ using UnityStandardAssets.Characters.ThirdPerson;
 public class Enemy : MonoBehaviour, IDamagable {
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackDamagePerShot = 9f;
     [SerializeField] float attackRadius = 4f;
-    
+    [SerializeField] float chaseRadius = 8f;
+    [SerializeField] GameObject projectileToUse;
+    [SerializeField] GameObject projectileSocket;
+
     float currentHealthPoints = 100f;
     AICharacterControl aiCharacterControl = null;
     GameObject player = null;
@@ -37,6 +41,11 @@ public class Enemy : MonoBehaviour, IDamagable {
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         if (distanceToPlayer <= attackRadius)
         {
+            SpawnProjectile();            
+        }
+
+        if (distanceToPlayer <= chaseRadius)
+        {
             aiCharacterControl.SetTarget(player.transform);
         }
         else
@@ -45,4 +54,26 @@ public class Enemy : MonoBehaviour, IDamagable {
         }
     }
 
+    void SpawnProjectile()
+    {
+        GameObject newProjectile = Instantiate(projectileToUse, projectileSocket.transform.position, Quaternion.identity);
+        Projectile projectileComponent = newProjectile.GetComponent<Projectile>();
+        projectileComponent.damageCaused = attackDamagePerShot;
+
+        Vector3 unitVectorToPlayer = (player.transform.position - projectileSocket.transform.position).normalized;
+        float projectileSpeed = projectileComponent.projectileSpeed;
+        newProjectile.GetComponent<Rigidbody>().velocity = unitVectorToPlayer * projectileSpeed;
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Draw chase gizmos
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+
+        //Draw attack gizmos
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+    }
 }
