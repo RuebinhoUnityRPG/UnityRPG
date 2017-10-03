9977576cda5044c750e383b0f5369c7aab7130d4
Dennis Rübner <dennis.ruebner@gmail.com>
2017-10-03 19:59:30 +0200

    level flow tested and healers added

---
+++ Assets/Enemies/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float damageCaused;
+    public float projectileSpeed;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Component damagableComponent = collider.gameObject.GetComponent(typeof(IDamagable));
+        if(damagableComponent)
+        {
+            (damagableComponent as IDamagable).TakeDamage(damageCaused);
+            Destroy(gameObject);
+        }
+    }
+}
