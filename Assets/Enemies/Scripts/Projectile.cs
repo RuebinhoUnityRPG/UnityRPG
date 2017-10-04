3521a264aeb5f7dbad806f988b9c0e63415af81f
Dennis Rübner <dennis.ruebner@gmail.com>
2017-10-04 10:02:45 +0200

    cleaned up enemy folder

--- Assets/Enemies/Scripts/Projectile.cs
+++ Assets/Enemies/Scripts/Projectile.cs
@@ -7,12 +7,33 @@ public class Projectile : MonoBehaviour {
     public float damageCaused;
     public float projectileSpeed;
 
+    private Vector3 startPosition;
+    private float xPosEnd = 20;
+    private float yPosEnd = 20;
+    private float zPosEnd = 20;
+
+    private void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         Component damagableComponent = collider.gameObject.GetComponent(typeof(IDamagable));
         if(damagableComponent)
         {
             (damagableComponent as IDamagable).TakeDamage(damageCaused);
+            Destroy(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if ((transform.position.x > (startPosition.x + xPosEnd)) || (transform.position.y > (startPosition.y + yPosEnd)) || (transform.position.y > (startPosition.y + yPosEnd)))
+        {
             Destroy(gameObject);
         }
     }
